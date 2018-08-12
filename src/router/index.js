8fd7f7a8abf2d08fd8069981f1c03b5eeb7f520a
Palayoub <nettimes1@gmail.com>
2018-08-12 21:49:53 +0100

    []: Router fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,19 @@
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import Vue from 'vue'
-import Home from '@/components/Home'
-import Admin from '@/components/admin/Admin'
-import Article from '@/components/Article'
+import Home from '../components/Home'
+import Admin from '../components/admin/Admin'
+import Article from '../components/Article'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
 const routes = [
-  { path: '/ok', component: Home, name: 'Home' },
+  { path: '/', component: Home, name: 'Home' },
   { path: '/home', component: Home, name: 'Home' },
   { path: '/addarticle', component: Article, name: 'Article' },
   { path: '/admin', component: Admin, name: 'Admin' }
 ]
 
-export default new Router({
-  routes : [
-    { path: '/Home', component: Home, name: 'Home' },
-    { path: '/Article', component: Article, name: 'Article' },
-    { path: '/Admin', component: Admin, name: 'Admin' }
-  ]
-});
-
-
+export default new VueRouter({
+  mode: 'history',
+  routes // short for `routes: routes`
+})
